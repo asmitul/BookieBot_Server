02c5b641a381d4c605a665e17ffe5bbbc68019d8
asmitul <asmitul@users.noreply.github.com>
2024-06-10 09:53:14 +0300

    add try except block

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, FastAPI, Depends, HTTPException, Path, Body, Query
+from fastapi import APIRouter, FastAPI, Depends, HTTPException, status
 
 from .models import AccountCreate, AccountResponseModel, GetAccountsResponseModel, ResponseModel, TransactionCreate, TransactionResponseModel, TransactionsResponseModel
 from .database import mongodb
@@ -9,21 +9,27 @@ app = FastAPI()
 router = APIRouter(prefix="/v1")
 
 # Accounts
-@router.post("/accounts/", dependencies=[Depends(get_api_key)], tags=["Accounts"], response_model=AccountResponseModel)
+@router.post("/accounts/", dependencies=[Depends(get_api_key)], tags=["Accounts"], response_model=AccountResponseModel, status_code=status.HTTP_201_CREATED)
 async def create_account(account: AccountCreate):
-    result = await mongodb.db["accounts"].insert_one(account.dict())
-    created_account = await mongodb.db["accounts"].find_one({"_id": result.inserted_id})
-    if not created_account:
-        raise HTTPException(status_code=404, detail="Account creation failed")
+    try:
+        result = await mongodb.db["accounts"].insert_one(account.dict())
+        created_account = await mongodb.db["accounts"].find_one({"_id": result.inserted_id})
+        if not created_account:
+            raise HTTPException(status_code=404, detail="Account creation failed")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     return AccountResponseModel.from_mongo(created_account)
 
 
 @router.get("/accounts/", dependencies=[Depends(get_api_key)], tags=["Accounts"], response_model=GetAccountsResponseModel)
 async def get_accounts():
     accounts = []
-    async for account in mongodb.db["accounts"].find():
-        account['_id'] = str(account['_id'])  # Convert _id to str
-        accounts.append(AccountResponseModel.from_mongo(account))
+    try:
+        async for account in mongodb.db["accounts"].find():
+            account['_id'] = str(account['_id'])  # Convert _id to str
+            accounts.append(AccountResponseModel.from_mongo(account))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     return GetAccountsResponseModel(accounts=accounts)
 
 
@@ -31,9 +37,12 @@ async def get_accounts():
 async def get_account(account_id: str):
     if not ObjectId.is_valid(account_id):
         raise HTTPException(status_code=400, detail="Invalid ObjectId")
-    account = await mongodb.db["accounts"].find_one({"_id": ObjectId(account_id)})
-    if account is None:
-        raise HTTPException(status_code=404, detail="Account not found")
+    try:
+        account = await mongodb.db["accounts"].find_one({"_id": ObjectId(account_id)})
+        if account is None:
+            raise HTTPException(status_code=404, detail="Account not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     return AccountResponseModel.from_mongo(account)
 
 
@@ -41,12 +50,15 @@ async def get_account(account_id: str):
 async def update_account(account_id: str, account: AccountCreate):
     if not ObjectId.is_valid(account_id):
         raise HTTPException(status_code=400, detail="Invalid ObjectId")
-    result = await mongodb.db["accounts"].update_one({"_id": ObjectId(account_id)}, {"$set": account.dict()})
-    if result.matched_count == 0:
-        raise HTTPException(status_code=404, detail="Account not found")
-    updated_account = await mongodb.db["accounts"].find_one({"_id": ObjectId(account_id)})
-    if updated_account is None:
-        raise HTTPException(status_code=404, detail="Account not found")
+    try:
+        result = await mongodb.db["accounts"].update_one({"_id": ObjectId(account_id)}, {"$set": account.dict()})
+        if result.matched_count == 0:
+            raise HTTPException(status_code=404, detail="Account not found")
+        updated_account = await mongodb.db["accounts"].find_one({"_id": ObjectId(account_id)})
+        if updated_account is None:
+            raise HTTPException(status_code=404, detail="Account not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     return AccountResponseModel.from_mongo(updated_account)
 
 
@@ -54,9 +66,12 @@ async def update_account(account_id: str, account: AccountCreate):
 async def delete_account(account_id: str):
     if not ObjectId.is_valid(account_id):
         raise HTTPException(status_code=400, detail="Invalid ObjectId")
-    result = await mongodb.db["accounts"].delete_one({"_id": ObjectId(account_id)})
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Account not found")
+    try:
+        result = await mongodb.db["accounts"].delete_one({"_id": ObjectId(account_id)})
+        if result.deleted_count == 0:
+            raise HTTPException(status_code=404, detail="Account not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     return ResponseModel(success=True, message="Account deleted")
 
 
@@ -64,46 +79,57 @@ async def delete_account(account_id: str):
 
 @router.post("/transactions/", dependencies=[Depends(get_api_key)], tags=["Transactions"], response_model=TransactionResponseModel)
 async def create_transaction(transaction: TransactionCreate):
-    transaction.serialNumber = await TransactionCreate._generate_serial_number()
-    result = await mongodb.db["transactions"].insert_one(transaction.dict())
-    if not result.inserted_id:
-        raise HTTPException(status_code=500, detail="Transaction creation failed")
-        
-    # Retrieve the created transaction from the database
-    created_transaction = await mongodb.db["transactions"].find_one({"_id": result.inserted_id})
-    if not created_transaction:
-        raise HTTPException(status_code=404, detail="Failed to retrieve created transaction")
+    try:
+        transaction.serialNumber = await TransactionCreate._generate_serial_number()
+        result = await mongodb.db["transactions"].insert_one(transaction.dict())
+        if not result.inserted_id:
+            raise HTTPException(status_code=500, detail="Transaction creation failed")
+            
+        # Retrieve the created transaction from the database
+        created_transaction = await mongodb.db["transactions"].find_one({"_id": result.inserted_id})
+        if not created_transaction:
+            raise HTTPException(status_code=404, detail="Failed to retrieve created transaction")
 
-    return TransactionResponseModel(**created_transaction)
+        return TransactionResponseModel(**created_transaction)
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.get("/transactions/", dependencies=[Depends(get_api_key)], tags=["Transactions"], response_model=TransactionsResponseModel)
 async def get_transactions():
-    transactions = await mongodb.db["transactions"].find().to_list(length=None)
-    if not transactions:
-        raise HTTPException(status_code=404, detail="No transactions found")
+    try:
+        transactions = await mongodb.db["transactions"].find().to_list(length=None)
+        if not transactions:
+            raise HTTPException(status_code=404, detail="No transactions found")
 
-    # Convert _id to str and create TransactionResponseModel instances
-    transactions_response = [TransactionResponseModel(**{**transaction, '_id': str(transaction['_id'])}) for transaction in transactions]
+        # Convert _id to str and create TransactionResponseModel instances
+        transactions_response = [TransactionResponseModel(**{**transaction, '_id': str(transaction['_id'])}) for transaction in transactions]
 
-    return TransactionsResponseModel(transactions=transactions_response)
+        return TransactionsResponseModel(transactions=transactions_response)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.get("/transactions/{serial_number}", dependencies=[Depends(get_api_key)], tags=["Transactions"], response_model=TransactionResponseModel)
 async def get_transaction(serial_number: int):
-    
-    transaction = await mongodb.db["transactions"].find_one({"serialNumber": serial_number})
-    if transaction is None:
-        raise HTTPException(status_code=404, detail="Transaction not found")
-    return TransactionResponseModel(**transaction)
+    try:
+        transaction = await mongodb.db["transactions"].find_one({"serialNumber": serial_number})
+        if transaction is None:
+            raise HTTPException(status_code=404, detail="Transaction not found")
+        return TransactionResponseModel(**transaction)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 @router.delete("/transactions/{serial_number}", dependencies=[Depends(get_api_key)], tags=["Transactions"], response_model=ResponseModel)
 async def delete_transaction(serial_number: int):
-    result = await mongodb.db["transactions"].delete_one({"serialNumber": serial_number})
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Transaction not found")
-    return ResponseModel(success=True, message="Transaction deleted")
-
+    try:
+        result = await mongodb.db["transactions"].delete_one({"serialNumber": serial_number})
+        if result.deleted_count == 0:
+            raise HTTPException(status_code=404, detail="Transaction not found")
+        return ResponseModel(success=True, message="Transaction deleted")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 app.include_router(router)
