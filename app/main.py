a53f54ea0e26cd4a1a463b5e7b8d9bc2c7a4ac53
asmitul <asmitullah@gmail.com>
2024-08-05 10:37:57 +0300

    tmp

--- app/main.py
+++ app/main.py
@@ -579,6 +579,116 @@ async def find_returns(
     rate_list.reverse()
     return rate_list
 
+@router.get("/tefas/Ayda_KODa_500_yatirsam_tmp/{fonkod}", tags=["Tefas"])
+async def find_returns(
+    fonkod: str = Path(..., description="Fund code"),
+    ay_sayisi: int = Query(None, description="Kac ay olsun?"),
+):
+    print("Running find_returns")
+
+    today = datetime.today()
+
+    rate_list = []
+
+    for i in range(ay_sayisi):
+        print(f"Getting data for month {i+1}")
+        month_offset = today.month - (i + 1)
+        year = today.year + (month_offset // 12)
+        month = month_offset % 12
+        if month <= 0:
+            month += 12
+            year -= 1
+
+        # 计算该月的第一天和最后一天
+        first_day = datetime(year, month, 1)
+        last_day = datetime(year, month, calendar.monthrange(year, month)[1])
+        
+        # 格式化日期
+        first_day_str = first_day.strftime('%d.%m.%Y')
+        last_day_str = last_day.strftime('%d.%m.%Y')
+
+        payload = ComparisonFundReturnsRequest(
+            bastarih= first_day_str,
+            bittarih= last_day_str
+        )
+
+        url = 'https://www.tefas.gov.tr/api/DB/BindComparisonFundReturns'
+
+        try:
+            response = requests.post(url, data=payload.dict())
+            response.raise_for_status()
+            data = response.json()
+
+            # 在 data["data"] 中的 FONKODU 如果没有响应的fonkod ， 那么跳出上面的for语句
+            found = False
+
+            for item in data["data"]:
+                if item["FONKODU"] == fonkod:
+                    rate_list.append(item["GETIRIORANI"])
+                    found = True
+                    break
+            
+            if not found:
+                break
+
+        except requests.exceptions.HTTPError as http_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"HTTP error occurred: {http_err}")
+        except requests.exceptions.ConnectionError as conn_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Connection error occurred: {conn_err}")
+        except requests.exceptions.Timeout as timeout_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Timeout error occurred: {timeout_err}")
+        except requests.exceptions.RequestException as req_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {req_err}")
+
+        
+
+    # 倒序列表
+    rate_list.reverse()
+
+    print(rate_list)
+
+    # delete None in list
+    rate_list = list(filter(None, rate_list))
+
+    print(rate_list)
+
+
+    period_profits = rate_list
+    # 每周投资金额
+    investment_per_peroid = 100
+
+    # 初始投资账户价值
+    investment_value = 0
+
+    # 总投资金额
+    total_investment = 0
+
+    # 遍历每周的利润率并更新投资账户价值
+    for profit in period_profits:
+        # 将当前投资金额加入账户
+        investment_value += investment_per_peroid
+        # 更新总投资金额
+        total_investment += investment_per_peroid
+        # 根据当前周的利润率更新账户价值
+        investment_value += investment_value * (profit / 100)
+        print(f"当前账户价值: {total_investment} - {investment_value:.2f} 元")
+
+    # 计算最终利润
+    final_profit = investment_value - total_investment
+
+    print(f"总共投入: {total_investment:.2f} 元")
+    print(f"最终利润: {final_profit:.2f} 元")
+
+    # 避免除以零的错误
+    if total_investment != 0:
+        profit_rate = (final_profit / total_investment) * 100
+        print(f"利润率: {profit_rate:.2f}%")
+        print(f"period%: {profit_rate/len(period_profits):.2f}%")
+    else:
+        print("总投入为 0，无法计算利润率")
+
+    return total_investment, final_profit, profit_rate, profit_rate/len(period_profits)
+
 # fon adet degisimi
 @router.get("/tefas/FonAdetDegisimi/{fonkod}", tags=["Tefas"])
 async def fon_adet_degisimi(
@@ -634,11 +744,6 @@ async def fon_adet_degisimi(
         except requests.exceptions.RequestException as req_err:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {req_err}")
 
-
-
-
-
-
         current_date -= timedelta(days=1)
 
     
@@ -649,5 +754,7 @@ async def fon_adet_degisimi(
     FIYAT_LIST.reverse()
     
     return FIYAT_LIST, TEDPAYSAYISI_LIST, KISISAYISI_LIST, PORTFOYBUYUKLUK_LIST
+
+# return fons history price as long as possible and save to database, get USDTRY price
 
 app.include_router(router)
