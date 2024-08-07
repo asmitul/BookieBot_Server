2672b2551615c21aab1a240ed9da802a57979ac7
asmitul <asmitullah@gmail.com>
2024-08-07 17:16:58 +0300

    tmp

--- app/main.py
+++ app/main.py
@@ -833,6 +833,7 @@ async def find_returns(
         else:
             print("总投入为 0，无法计算利润率")
 
+        data__['fonkod'] = fonkod
         data__['total_investment'] = total_investment
         data__['final_profit'] = final_profit
         data__['profit_rate'] = profit_rate
@@ -842,6 +843,160 @@ async def find_returns(
 
     return data_total
 
+
+@router.get("/tefas/tum_hisse_senedi_fonlari_getirisi_v2", tags=["Tefas"])
+async def find_returns(
+    ay_sayisi: int = Query(None, description="Kac ay olsun?"),
+):
+    print("Running find_returns")
+
+    today = datetime.today()
+
+    # fon list
+    fon_list = []
+        
+    payload = ComparisonFundReturnsRequest(
+        bastarih=(datetime.now() - timedelta(days=30)).strftime('%d.%m.%Y'),
+        bittarih=datetime.now().strftime('%d.%m.%Y')
+    )
+
+    url = 'https://www.tefas.gov.tr/api/DB/BindComparisonFundReturns'
+
+    
+    response = requests.post(url, data=payload.dict())
+    response.raise_for_status()
+    data = response.json()
+
+    # 处理 GETIRIORANI 为 None 的情况，将 None 替换为一个最小的值（如负无穷）
+    for item in data['data']:
+        if item['GETIRIORANI'] is None:
+            item['GETIRIORANI'] = -1e10
+
+    # 按照 GETIRIORANI 从大到小排序
+    sorted_data = sorted(data['data'], key=lambda x: x['GETIRIORANI'], reverse=True)
+
+    # 更新原数据
+    data['data'] = sorted_data
+
+    for item in data['data']:
+        fon_list.append(item['FONKODU'])
+
+    # print(fon_list)
+
+    # fon_list = ["IIH","MAC","YAS"]
+
+    ##################################################
+
+    dic_A = {}
+    for i in range(ay_sayisi):
+
+        print(f"Getting data for month {i+1}")
+        month_offset = today.month - (i + 1)
+        year = today.year + (month_offset // 12)
+        month = month_offset % 12
+        if month <= 0:
+            month += 12
+            year -= 1
+
+        # 计算该月的第一天和最后一天
+        first_day = datetime(year, month, 1)
+        last_day = datetime(year, month, calendar.monthrange(year, month)[1])
+        
+        # 格式化日期
+        first_day_str = first_day.strftime('%d.%m.%Y')
+        last_day_str = last_day.strftime('%d.%m.%Y')
+
+        payload = ComparisonFundReturnsRequest(
+            bastarih= first_day_str,
+            bittarih= last_day_str
+        )
+
+        url = 'https://www.tefas.gov.tr/api/DB/BindComparisonFundReturns'
+
+        try:
+            response = requests.post(url, data=payload.dict())
+            response.raise_for_status()
+            data = response.json()
+
+            dic_A[i+1] = data
+
+
+        except requests.exceptions.HTTPError as http_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"HTTP error occurred: {http_err}")
+        except requests.exceptions.ConnectionError as conn_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Connection error occurred: {conn_err}")
+        except requests.exceptions.Timeout as timeout_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Timeout error occurred: {timeout_err}")
+        except requests.exceptions.RequestException as req_err:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {req_err}")
+
+        
+
+    # fon_list = ["YAS","MAC","IIH"]
+
+    data_B = {}
+    for fon in fon_list:
+        print("fon: ", fon)
+
+        list_b = []
+
+        for key, value in dic_A.items():
+            for item in value['data']:
+                if item['FONKODU'] == fon:
+                    list_b.append(item['GETIRIORANI'])
+
+        data_B[fon] = list_b
+
+
+    data_C = {}
+    for fonkod, list_c in data_B.items():
+        list_d = []
+        list_c.reverse()
+        list_c = list(filter(None, list_c))
+
+        period_profits = list_c
+        # 每周投资金额
+        investment_per_peroid = 100
+
+        # 初始投资账户价值
+        investment_value = 0
+
+        # 总投资金额
+        total_investment = 0
+
+        # 遍历每周的利润率并更新投资账户价值
+        for profit in period_profits:
+            # 将当前投资金额加入账户
+            investment_value += investment_per_peroid
+            # 更新总投资金额
+            total_investment += investment_per_peroid
+            # 根据当前周的利润率更新账户价值
+            investment_value += investment_value * (profit / 100)
+            print(f"当前账户价值: {total_investment} - {investment_value:.2f} 元")
+
+        # 计算最终利润
+        final_profit = investment_value - total_investment
+
+        print(f"总共投入: {total_investment:.2f} 元")
+        print(f"最终利润: {final_profit:.2f} 元")
+
+        # 避免除以零的错误
+        if total_investment != 0:
+            profit_rate = (final_profit / total_investment) * 100
+            print(f"利润率: {profit_rate:.2f}%")
+            print(f"period%: {profit_rate/len(period_profits):.2f}%")
+        else:
+            print("总投入为 0，无法计算利润率")
+
+        list_d.append(total_investment)
+        list_d.append(investment_value)
+        list_d.append(profit_rate)
+        list_d.append(profit_rate/len(period_profits))
+
+        data_C[fonkod] = list_d
+
+    sorted_data = dict(sorted(data_C.items(), key=lambda item: item[1][3], reverse=True))
+    return sorted_data
 
 # fon adet degisimi
 @router.get("/tefas/FonAdetDegisimi/{fonkod}", tags=["Tefas"])
